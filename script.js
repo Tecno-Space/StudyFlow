a8bd776d5780bccd210484ea482cc865577558ab
Tecno-Space <isatrinipau@gmail.com>
2026-03-04 20:25:58 -0400

    Mejora en deletetask

--- script.js
+++ script.js
@@ -73,8 +73,6 @@ taskForm.addEventListener("submit", function (e) {
 // Eliminar tarea
 function deleteTask(id) {
     
-    alert("Se ejecutó la función");
-
     if (confirm("¿Estás seguro de que quieres eliminar esta tarea?")) {
         tasks = tasks.filter(task => task.id !== id);
         saveTasks();
